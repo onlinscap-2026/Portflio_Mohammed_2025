aec1ef8fd7a9d84994048349442430532910df7b
onlinscap-2026 <onlinscap@gmail.com>
2026-03-26 00:07:27 +0300

    update

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -22,7 +22,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
     setLoading(true);
     try {
       // const response = await fetch('https://my-portofolio-5pxn.onrender.com/login', {
-      const response = await fetch('https://portoflio-mohammed-2025.onrender.com/Login', {
+      const response = await fetch('https://portoflio-mohammed-2025.onrender.com/login', {
         //const response = await fetch('http://localhost:10000/Login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
